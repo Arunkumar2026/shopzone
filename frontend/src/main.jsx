1a784fea47491c96f12b4ff7fc4e32fa4a84e6a0
Arun Kumar <akulakumar453@gmail.com>
2026-06-03 19:44:34 +0530

    this is frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import CartProvider from './context/CartContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <Toaster position='top-center'/> 
+      <Toaster position='top-left'/> 
       <App />
     </CartProvider>
   </React.StrictMode>,
