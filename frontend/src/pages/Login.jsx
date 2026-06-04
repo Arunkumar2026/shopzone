31570daba4ed8cf04d5e60151fe2859a1f0ce243
Arun Kumar <akulakumar453@gmail.com>
2026-06-04 17:31:21 +0530

    this is frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,38 @@
-
+import { useState } from "react";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
   return (
-    <div>Login</div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-6">login</h1>
+        <div className="mb-4">
+          <label className="block mb-2 font-semibold">
+            Email
+          </label>
+          <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border px-4 py-2 rounded outline-none focus:ring-2 focus:ring-black" required />
+        </div>
+
+        <div className="mb-6">
+          <label className="block mb-2 font-semibold">password</label>
+          <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full border px-4 py-2 rounded outline-none focus:ring-2 focus:ring-black" required />
+        </div>
+
+        <button type="submit" className="w-full bg-black text-white py-2 rounded hover:bg-gray-800 transition">Login</button>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,42 @@
-
+import { useState } from "react";
 
 const Signup = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      name, email, password, 
+    });
+    
+  };
+
   return (
-    <div>Signup</div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-6">Signup</h1>
+        <div className="mb-4">
+          <label className="block mb-2 font-semibold">Name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter your name" required className="w-full border px-4 py-2 rounded outline-none focus:ring-2 focus:ring-black" />
+        </div>
+
+        <div className="mb-4">
+          <label className="block mb-2 font-semibold">Email</label>
+          <input value={email} onChange={(e) => setEamil(e.target.value)} type="email" required placeholder="Enter you email" className="w-full px-4 py-2 border rounded outline-none focus:ring-2 focus:ring-black"/>
+        </div>
+
+        <div className="mb-4">
+          <label className="block mb-2 font-semibold">Password</label>
+          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" required placeholder="Enter your password" className="w-full px-4 py-2 rounded border outline-none focus:ring-2 focus:ring-black"/>
+        </div>
+
+        <button type="submit" className="w-full bg-black text-white py-2 rounded hover:bg-gray-800 transition">Singup</button>
+      </form>
+    </div>
   )
 }
 
